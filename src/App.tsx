5109cb1a05a11f2303cfefbe03f41c65ff61015e
Kubrick Code <kubrickcode@gmail.com>
2023-06-24 23:26:03 +0900

    fix: vite 프록시 설정 및 서버 host 변경

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import axios from "axios";
 import { useState } from "react";
 import { FC } from "react";
 
-const host = import.meta.env.VITE_SERVER_HOST;
-
 const App: FC = () => {
   const [message, setMessage] = useState("");
   const onRequest = async () => {
-    const result = await axios.get(host);
+    const result = await axios.get(window.location.origin + "/api");
     setMessage(result.data);
   };
 
