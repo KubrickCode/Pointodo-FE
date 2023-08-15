7f03b2433a329c08169d963649eeb6217b17db67
Kubrick Code <kubrickcode@gmail.com>
2023-08-15 23:12:35 +0900

    feat: 포인트 정책 추가

--- src/components/Layout/Navigation/Navigation.tsx
+++ src/components/Layout/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import ProfileDropdown from "./ProfileDropdown";
 import { useQueryGet } from "../../../hooks/useQueryApi";
 import { useUserStore } from "../../../store/user.store";
@@ -8,6 +8,8 @@ const Navigation: FC = () => {
   const { data: currentPoints } = useQueryGet("/point/current", "getPoints", {
     enabled: !!isLoggedIn,
   });
+
+  const [pointInfo, setPointInfo] = useState(false);
 
   return (
     <>
@@ -25,7 +27,47 @@ const Navigation: FC = () => {
           </button>
         </div>
         <div className="flex flex-row items-center">
-          <div className="mr-2">보유 포인트: {currentPoints?.points}</div>
+          <div className="mr-2 flex flex-row relative">
+            <div
+              className="mr-1"
+              onMouseOver={() => setPointInfo(true)}
+              onMouseLeave={() => setPointInfo(false)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="gray"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+                />
+              </svg>
+              <div
+                className={`absolute border bg-white p-4 rounded w-fit right-0 top-8 ${
+                  !pointInfo && "hidden"
+                }`}
+              >
+                <p className="text-lg mb-2">※ 포인트 정책</p>
+                <p className="whitespace-nowrap">- 매일 작업 완료 시 1포인트</p>
+                <p className="whitespace-nowrap">- 기한 작업 완료 시 3포인트</p>
+                <p className="whitespace-nowrap">
+                  - 무기한 작업 완료 시 5포인트
+                </p>
+                <p className="whitespace-nowrap">
+                  - 이틀 이상 연속으로 작업 완료 시 추가 1포인트
+                </p>
+                <p className="whitespace-nowrap">
+                  - 포인트로 각종 뱃지 구매 가능
+                </p>
+              </div>
+            </div>
+            <span>보유 포인트: {currentPoints?.points}</span>
+          </div>
           <ProfileDropdown />
         </div>
       </nav>
