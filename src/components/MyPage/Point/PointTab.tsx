a61852ae01168823855130a239647bf4caa39c94
Kubrick Code <kubrickcode@gmail.com>
2023-08-27 21:31:28 +0900

    feat: PointTab map함수 최적화

--- src/components/MyPage/Point/PointTab.tsx
+++ src/components/MyPage/Point/PointTab.tsx
@@ -9,25 +9,31 @@ const PointTab: FC<Props> = ({ tab, setTab }) => {
   return (
     <>
       <ul className="flex flex-row justify-evenly border-b">
-        <li
-          onClick={() => setTab(0)}
-          className={`${
-            tab === 0 && "bg-neutral-200"
-          } w-full text-center p-5 cursor-pointer`}
-        >
-          적립 포인트 내역
-        </li>
-        <li
-          onClick={() => setTab(1)}
-          className={`${
-            tab === 1 && "bg-neutral-200"
-          } w-full text-center p-5 cursor-pointer`}
-        >
-          소모 포인트 내역
-        </li>
+        {pointTabList.map((item) => (
+          <li
+            key={item.id}
+            onClick={() => setTab(item.id)}
+            className={`${
+              tab === item.id && "bg-neutral-200"
+            } w-full text-center p-5 cursor-pointer`}
+          >
+            {item.name}
+          </li>
+        ))}
       </ul>
     </>
   );
 };
+
+const pointTabList = [
+  {
+    id: 0,
+    name: "적립 포인트 내역",
+  },
+  {
+    id: 1,
+    name: "소모 포인트 내역",
+  },
+];
 
 export default PointTab;
