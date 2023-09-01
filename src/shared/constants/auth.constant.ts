64631f2d5dd8eff3d37949ae846c9091390765db
Kubrick Code <kubrickcode@gmail.com>
2023-09-01 20:46:37 +0900

    fix: user api users로 모두 변경

--- src/shared/constants/auth.constant.ts
+++ src/shared/constants/auth.constant.ts
@@ -1,5 +1,5 @@
 export const LOGIN_LINK = "/auth/login";
-export const REGISTER_LINK = "/user/register";
+export const REGISTER_LINK = "/users/register";
 
 export const CHECK_PASSWORD_LINK = "/auth/check-password";
 export const LOGOUT_LINK = "/auth/logout";
