38c8fe0b147a73bf435dcd50b69c64130bfe48eb
Kubrick Code <kubrickcode@gmail.com>
2023-09-02 13:37:31 +0900

    fix: GET_TASK 작업 limit 추가

--- src/shared/constants/task.constant.ts
+++ src/shared/constants/task.constant.ts
@@ -8,7 +8,8 @@ export const GET_TASK_LINK = (
   taskType: TaskType,
   currentPage: number,
   order: string
-) => `/tasks?taskType=${taskType}&page=${currentPage}&order=${order}`;
+) =>
+  `/tasks?taskType=${taskType}&offset=${currentPage}&limit=${TASK_LIMIT_IN_A_PAGE}&order=${order}`;
 export const GET_TASK_TOTAL_PAGE = (taskType: string) =>
   `/tasks/count/${taskType}`;
 export const COMPLETE_TASK_LINK = (id: number) => `/tasks/complete/${id}`;
@@ -17,3 +18,5 @@ export const CANCLE_TASK_LINK = (id: number) => `/tasks/cancle/${id}`;
 export const DAILY_TASK = "매일 작업";
 export const DUE_TASK = "기한 작업";
 export const FREE_TASK = "무기한 작업";
+
+export const TASK_LIMIT_IN_A_PAGE = 5;
