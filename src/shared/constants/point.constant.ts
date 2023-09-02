b972569be5eb1aa68e331206972661dcba24d1be
Kubrick Code <kubrickcode@gmail.com>
2023-09-02 15:13:15 +0900

    fix: point api 경로 points 로 변경

--- src/shared/constants/point.constant.ts
+++ src/shared/constants/point.constant.ts
@@ -1,8 +1,8 @@
-export const GET_CURRENT_POINTS_LINK = "/point/current";
+export const GET_CURRENT_POINTS_LINK = "/points/current";
 export const GET_POINTS_LOGS_LINK = (
   type: string,
   currentPage: number,
   order: string
-) => `/point/logs/${type}?page=${currentPage}&order=${order}`;
+) => `/points/logs/${type}?page=${currentPage}&order=${order}`;
 export const GET_POINTS_LOGS_TOTAL_PAGES = (type: string) =>
-  `/point/count/${type}`;
+  `/points/count/${type}`;
