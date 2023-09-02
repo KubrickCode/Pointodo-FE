1ffa9a5763cfd7beaf61676fb25386f70a8784cb
Kubrick Code <kubrickcode@gmail.com>
2023-09-02 16:35:30 +0900

    feat: 선택 뱃지 변경 api 경로 변경

--- src/shared/constants/badge.constant.ts
+++ src/shared/constants/badge.constant.ts
@@ -1,5 +1,6 @@
 export const BUY_BADGE_LINK = (badgeId: number) => `/badges/${badgeId}`;
-export const GET_ALL_BADGE_LIST_LINK = "/badges/all";
+export const GET_ALL_BADGE_LIST_LINK = "/badges";
 export const GET_MY_BADGE_LIST_LINK = "/badges/personal";
 export const GET_MY_BADGE_PROGRESS_LINK = "/badges/progress";
-export const CHANGE_SELECTED_BADGE_LINK = "/badges/selected";
+export const CHANGE_SELECTED_BADGE_LINK = (badgeId: number) =>
+  `/badges/${badgeId}`;
